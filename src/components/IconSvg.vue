ef394b68dd74d91c7a7e8c8ad7699ba5159ce268
Nikolay Chemov <trustforever@mail.ru>
2022-05-27 19:06:11 +0300

    add new eslint rules

--- src/components/IconSvg.vue
+++ src/components/IconSvg.vue
@@ -5,11 +5,17 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 
+onMounted(() => {
+
+});
 </script>
 
 <style lang="scss" module>
 .icon {
   color: inherit;
+  height: auto;
+  width: auto;
 }
 </style>
