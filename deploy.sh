bf2234ebf0b416e0b3bbf343253a59ed17aad77d
Nikolay Chemov <trustforever@mail.ru>
2022-06-03 23:21:30 +0300

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com-chok8356:chok8356/url-decoder.git main:gh-pages
+
+cd -
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig((args) => {
+  const base = args.mode === 'production'
+    ? '/url-decoder/'
+    : '/';
+
   const generateScopedName = args.mode === 'production'
     ? '[hash:base64:5]'
     : '[name]__[local]--[hash:base64:5]';
+
   return {
+    base,
     plugins: [
       vue(),
     ],
